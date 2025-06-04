ac67304c2d5da25496a20cdafa0277a7a9f5366b
Jacob Wilson <jacobtrwilson@icloud.com>
2025-06-04 18:13:58 -0500

    correct version number

--- merge-main.rb
+++ merge-main.rb
@@ -1,7 +1,7 @@
 class MergeMain < Formula
   desc "Used to merge the main branch into the current branch"
   homepage "https://github.com/Obi-Jacobi/merge-main"
-  url "https://github.com/Obi-Jacobi/merge-main/releases/download/0.1.0/mergeMain.zip"
+  url "https://github.com/Obi-Jacobi/merge-main/releases/download/0.0.1/mergeMain.zip"
   sha256 "sha256:82c235fe86c0694a575d415a88bc2e709fa7aafa9d33d91d727b6221eb2ceb32"
   version "0.0.1"
 
